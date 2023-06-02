1606a8c6706238a12d7f376cd8dc07f12248a559
lsh955 <lshk955@naver.com>
2023-06-02 12:49:19 +0900

    Build Script Update or komapper dependencies added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.8.21"
     application
+    id("com.google.devtools.ksp") version "1.8.21-1.0.11"
+    kotlin("jvm") version "1.8.21"
 }
 
 group = "org.example"
@@ -11,11 +12,23 @@ repositories {
 }
 
 dependencies {
+    val komapperVersion = "1.10.0"
+    platform("org.komapper:komapper-platform:$komapperVersion").let {
+        implementation(it)
+        ksp(it)
+    }
+    implementation("org.komapper:komapper-starter-jdbc")
+    implementation("org.komapper:komapper-dialect-h2-jdbc")
+    ksp("org.komapper:komapper-processor")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    withType<Test>().configureEach {
+        useJUnitPlatform()
+    }
 }
 
 kotlin {
@@ -24,4 +37,5 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+    mainClass.set("org.komapper.quickstart.ApplicationKt")
 }
